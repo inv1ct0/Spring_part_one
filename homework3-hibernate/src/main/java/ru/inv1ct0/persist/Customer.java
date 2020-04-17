bf359e9f1dbbf11f730d2852bff6d8cb73b7b15e
inv1ct0 <a.m.yudenkov@yandex.ru>
2020-04-18 01:03:25 +0300

    les_3

---
+++ homework3-hibernate/src/main/java/ru/inv1ct0/persist/Customer.java
@@ -0,0 +1,64 @@
+package ru.inv1ct0.persist;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255)
+    private String name;
+
+    @OneToMany(
+            mappedBy = "customer",
+            cascade = CascadeType.ALL
+    )
+
+    private List<Product> products;
+
+    public Customer() {
+    }
+
+    public Customer(String name, List<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", products=" + products +
+                '}';
+    }
+}
+
